e51d5faed5cd2969414b96ca1fa1051428594ea0
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-11-18 12:18:30 -0800

    completed intro-bg

--- src/components/IntroBackground.js
+++ src/components/IntroBackground.js
@@ -4,22 +4,11 @@ import React from "react";
 export default function IntroBackground() {
   return (
     <section className="h-screen m-0">
-      {/* <div className="">
-        <img
-          className="object-cover absolute"
-          style={{ maxHeight: "75vh", width: "100vw" }}
-          src="https://www.gannett-cdn.com/presto/2020/08/05/PNDN/5eafa3eb-816f-440f-b135-51bf36599a20-Romanzo_Interior_0809.jpg"
-        />
-        <div className="">
-          <Hero />
-        </div>
-      </div> */}
-
       <div className="hero-image">
-        <div className="hero-text">
-          <h1>I am John Doe</h1>
-          <p>And Im a Photographer</p>
-          <button>Hire me</button>
+        <div className="hero-text w-screen">
+          <h1 className="text-4xl md:text-6xl leading-8">Discover</h1>
+          <h1 className="text-4xl md:text-6xl ">Modern Villa</h1>
+          <button>Learn More</button>
         </div>
       </div>
     </section>
