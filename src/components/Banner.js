c6fbc0912c2a2078392764c1624e18d6033acc45
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-11-23 01:08:47 -0800

    padding updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ export default function Banner({firstLine,secondLine}) {
       <div className="banner-image">
         <div className="hero-text w-screen">
           <h1 className={`text-4xl md:text-6xl leading-8`}>{firstLine}</h1>
-          <h1 className="text-4xl md:text-6xl ">{secondLine}</h1>
+          <h1 className="text-3xl md:text-6xl ">{secondLine}</h1>
 
           <Button color="bg-indigo-600 text-white" text="Schedule a visit!" overallAddClass="hero-text button mt-12 md:mt-16" />
         </div>
